9c416ec3d7acdda50b9d5a65164d900f3e004762
benjaminsunderland <ben@sunderland.eu.com>
2017-10-07 17:36:20 +0100

    Added two tests for a 'gutter game' and 'rolling all ones'. Roll and score method included

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,9 +1,22 @@
-describe("The bowling game", function() {
+describe("The Bowling Game", function() {
 
-  it('can create game', function() {
-    var game = new BowlingGame()
+  var game;
 
+  beforeEach(function () {
+    game = new BowlingGame();
   });
 
+  it('can roll a gutter game', function() {
+    for (var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toBe(0);
+  });
 
+  it('can roll all ones', function() {
+    for (var i = 0; i < 20; i++) {
+      game.roll(1)
+    }
+    expect(game.score()).toBe(20);
+  });
 });
