780c7e90469db732e0b7b852d58d524d84fa7250
benjaminsunderland <ben@sunderland.eu.com>
2017-10-07 19:15:41 +0100

    Refactored code. Added three functions for a spare, a normal score and a spare score

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,17 +6,29 @@ describe("The Bowling Game", function() {
     game = new BowlingGame();
   });
 
-  it('can roll a gutter game', function() {
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
+  var rollMany = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      game.roll(pins);
     }
+
+  };
+
+  it('can roll a gutter game', function() {
+    rollMany(0, 20);
     expect(game.score()).toBe(0);
   });
 
   it('can roll all ones', function() {
-    for (var i = 0; i < 20; i++) {
-      game.roll(1)
-    }
+    rollMany(1, 20);
     expect(game.score()).toBe(20);
   });
+
+  it('can roll a spare', function() {
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    rollMany(0, 17);
+    expect(game.score()).toBe(16);
+  });
+
 });
